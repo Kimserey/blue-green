b95957068f446841f2bf61a22b4bab0d1f10327e
Kimserey <kimserey.lam@gmail.com>
2018-12-30 22:35:17 +0000

    Test

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ scp -qr ./frontend.zip notkl:~
 ssh notkl "unzip -o frontend.zip -d frontend"
 ssh notkl "sudo rm -rf /var/www/$DEPLOYMENT_ENV/frontend/*"
 ssh notkl "sudo mv ~/frontend/* /var/www/$DEPLOYMENT_ENV/frontend"
-ssh notkl "rm -f ~/frontend*"
+ssh notkl "rm -rf ~/frontend*"
