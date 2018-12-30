7748612f7deea0289759d62d449b7e09fd5efb91
Kimserey <kimserey.lam@gmail.com>
2018-12-30 21:41:39 +0000

    Added swap

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,11 @@
 
 set -ue
 
-DEPLOYMENT_ENV=$(curl https://staging.notkimsereylam.xyz/deployment_id)
+DEPLOYMENT_ENV=$(curl -s https://staging.notkimsereylam.xyz/deployment_id)
 
-echo $DEPLOYMENT_ENV
-
-# cd dist
-# scp -qr ./frontend.zip notkl:~/
-# ssh husky2 "unzip -o frontend.zip -d ~/frontend"
-# ssh husky2 "sudo rm -rf /*"
-# ssh husky2 "sudo cp -r ~/ek/$APP_NAME/* $APP_DIR"
-# ssh husky2 "sudo chown -R www-data:www-data $APP_DIR/*"
+cd dist
+scp -qr ./frontend.zip notkl:~
+ssh notkl "unzip -o frontend.zip -d frontend"
+ssh notkl "sudo rm -rf /var/www/$DEPLOYMENT_ENV/frontend/*"
+ssh notkl "sudo mv ~/frontend/* /var/www/$DEPLOYMENT_ENV/frontend"
+ssh notkl "rm -f ~/frontend*"
