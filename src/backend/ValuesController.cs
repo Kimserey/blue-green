ae37cf230de9a0369444e11e9ab7aad8e6f6d361
Kimserey <kimserey.lam@gmail.com>
2018-12-31 13:16:37 +0000

    Added example of backend

---
+++ src/backend/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController: ControllerBase
+    {
+        public ActionResult<string[]> Get()
+        {
+            return new[] { "Value 1", "Value 2", "Value 3" };
+        }
+    }
+}
